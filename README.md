228c45a4eaa849322e18a9a238148eaaf27bb17d
Günther Jena <guenther.jena@laseratwork.com>
2024-04-17 00:38:17 +0200

    speedup

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Sperry
-Parse, Gernerate and Manipulate KiCAD files
+Parse, generate and manipulate KiCAD files
 
 ## Goals
-* Able to read any (KiCAD) s-expression based files
+* [x] read any (KiCAD) s-expression based files (without fixed scheme)
+* [x] write parsed data back as s-expression
+* [ ] Methods to work with parsed s-expressions (create, read, update, delete)
+* [ ] registration of python classes to handle specific s-expressions
+* [ ] rules for line ending generation for writing s-expressions
+
+### Notes
 * Unknown expression should be abe to be read and manipulated
 * Schema of files and expressions are stored in JSON files
 * Python code with classes is generated from schema files
--- src/sperry/sexpr.py
+++ src/sperry/sexpr.py
@@ -5,93 +5,71 @@ from typing import Any
 
 match_whitespace = re.compile(r'\s+').match
 match_string = re.compile(r'"(?:\\.|[^"\\])*"').match
-match_number = re.compile(r'(-?(?:0|[1-9]\d*))(\.\d+)?([eE][-+]?\d+)?', (re.DOTALL)).match
 match_symbol = re.compile(r'[^\s)"]+').match
 
 WHITESPACE_STR = ' \t\n\r'
-
-class Symbol:
-    def __init__(self, value: str):
-        self.value = value
-    
-    def __repr__(self):
-        return f'{self.value}'
 
 
 class Expression:
     def __init__(self, elements: list):
         self.elements = elements
 
-    def __repr__(self):
-        return f'{self.__class__.__name__}({self.elements})'
-    
+    def __str__(self):
+        return '(' + ' '.join(str(e) for e in self.elements) + ')\n'
+
 
 def _parse(sexpr: str, index: int = 0) -> tuple[Any, int]:
     length = len(sexpr)
     elements = []
 
     while index < length:
+        character = sexpr[index]
         if sexpr[index] in WHITESPACE_STR:
-            index += len(match_whitespace(sexpr, index)[0])
+            index = match_whitespace(sexpr, index).end()
+            character = sexpr[index]
 
-        if sexpr[index] == '(':
+        if character == '(':
             element, index = _parse(sexpr, index + 1)
-        elif sexpr[index] == ')':
+        elif character == ')':
             return Expression(elements), index + 1
-        elif sexpr[index] == '"':
+        elif character == '"':
             match = match_string(sexpr, index)
-    
+
             if not match:
                 raise ValueError(f'Invalid string at {index}: {sexpr[index:index+12]}...')
-    
-            element = match[0][1:-1]
+
+            element = match[0]
             index = match.end()
         else:
-            # taken from simplejson
-            m = match_number(sexpr, index)
-            if m and sexpr[m.end()] in WHITESPACE_STR + ')':
-                integer, frac, exp = m.groups()
-                if frac or exp:
-                    element = float(integer + (frac or '') + (exp or ''))
-                else:
-                    element = int(integer)
-                index = m.end()
-            else:
-                m = match_symbol(sexpr, index)
-                if m:
-                    element = Symbol(m[0])
-                    index = m.end()
-                else:
-                    raise ValueError(f'Invalid expression at {index}: {sexpr[index:index+12]}...')
+            match = match_symbol(sexpr, index)
+            if not match:
+                raise ValueError(f'Invalid string at {index}: {sexpr[index:index+12]}...')
+
+            element = match[0]
+            index = match.end()
 
         elements.append(element)
-    
+
+        if sexpr[index] != ')':
+            index += 1
+
     if index != 0:
         raise ValueError('Invalid expression: unterminated')
 
     return elements[0], index
 
+
 def parse(sexpr: str) -> Any:
     assert sexpr[0] == '('
     return _parse(sexpr, 1)[0]
 
-def dump(obj: Any) -> str:
-    if isinstance(obj, Symbol):
-        return obj.value
-    elif isinstance(obj, Expression):
-        return '(' + ' '.join(dump(e) for e in obj.elements) + ')\n'
-    elif isinstance(obj, str):
-        return f'"{obj}"'
-    else:
-        return str(obj)
-                   
+
 if __name__ == '__main__':
     with open('/home/guenther/law/projects/labstack/pcb/pcb/labstack.kicad_pcb') as f:
         sexpr = f.read()
-    
+
     obj = parse(sexpr)
-    sexpr = dump(obj)
+    sexpr = str(obj)
 
     with open('dump.kicad_pcb', 'w') as f:
         f.write(sexpr)
-    
